07e8d0b82436bfea352425373ca34fe816752abf
Mostafa Hadian <mostafa.hadiandehkordi@sky.uk>
2016-08-15 14:27:14 +0100

    Add RecyclerViewInteraction

--- app/src/androidTest/java/com/karumi/katasuperheroes/SuperHeroesActivityTest.java
+++ app/src/androidTest/java/com/karumi/katasuperheroes/SuperHeroesActivityTest.java
@@ -17,14 +17,19 @@
 package com.karumi.katasuperheroes;
 
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
 
 import com.karumi.katasuperheroes.di.MainComponent;
 import com.karumi.katasuperheroes.di.MainModule;
+import com.karumi.katasuperheroes.matchers.ToolbarMatcher;
 import com.karumi.katasuperheroes.model.SuperHero;
 import com.karumi.katasuperheroes.model.SuperHeroesRepository;
+import com.karumi.katasuperheroes.recyclerview.RecyclerViewInteraction;
 import com.karumi.katasuperheroes.ui.view.SuperHeroesActivity;
 
 import org.junit.Rule;
@@ -32,6 +37,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 import it.cosenonjaviste.daggermock.DaggerMockRule;
@@ -40,6 +47,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.AllOf.allOf;
@@ -49,45 +57,105 @@ import static org.mockito.Mockito.when;
 @LargeTest
 public class SuperHeroesActivityTest {
 
-  @Rule public DaggerMockRule<MainComponent> daggerRule =
-      new DaggerMockRule<>(MainComponent.class, new MainModule()).set(
-          new DaggerMockRule.ComponentSetter<MainComponent>() {
-            @Override public void setComponent(MainComponent component) {SuperHeroesApplication app =
-                  (SuperHeroesApplication) InstrumentationRegistry.getInstrumentation()
-                      .getTargetContext()
-                      .getApplicationContext();
-              app.setComponent(component);
+    @Rule
+    public DaggerMockRule<MainComponent> daggerRule = new DaggerMockRule<>(MainComponent.class, new MainModule()).set(new DaggerMockRule.ComponentSetter<MainComponent>() {
+        @Override
+        public void setComponent(MainComponent component) {
+            SuperHeroesApplication app = (SuperHeroesApplication) InstrumentationRegistry.getInstrumentation().getTargetContext().getApplicationContext();
+            app.setComponent(component);
+        }
+    });
+
+    @Rule
+    public IntentsTestRule<SuperHeroesActivity> activityRule = new IntentsTestRule<>(SuperHeroesActivity.class, true, false);
+
+    @Mock
+    SuperHeroesRepository repository;
+
+    @Test
+    public void showsEmptyCaseIfThereAreNoSuperHeroes() {
+        givenThereAreNoSuperHeroes();
+
+        startActivity();
+
+        onView(withText("¯\\_(ツ)_/¯")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void showsTheToolbar() {
+        givenThereAreNoSuperHeroes();
+
+        startActivity();
+
+        //check if the toolbar is displayed and it has a childview with the mentioned text
+        onView(allOf(withId(R.id.toolbar), hasDescendant(withText("Kata Super Heroes")))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void showsTheToolbarWithToolbarMatcher() {
+        startActivity();
+        ToolbarMatcher.onToolbarWithTitle("Kata Super Heroes").check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void showsTheFirstItemWithCorrectData() {
+        givenThereIsASuperHeroes();
+        startActivity();
+        onView(withText("Mostafa")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void showsTheMultipleItemWithCorrectData() {
+        givenThereIsMultipleSuperHeroes();
+        startActivity();
+        onView(withText("Mostafa")).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.recycler_view), hasDescendant(withId(R.id.tv_super_hero_name)), hasDescendant(withText("Mostafa")))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void showsTheMultipleItemsWithCorrectData() {
+        givenThereIsMultipleSuperHeroes();
+        startActivity();
+        RecyclerViewInteraction.<SuperHero>onRecyclerView(withId(R.id.recycler_view)).withItems(repository.getAll()).check(new RecyclerViewInteraction.ItemViewAssertion<SuperHero>() {
+            @Override
+            public void check(SuperHero item, View view, NoMatchingViewException e) {
+                matches(hasDescendant(withText(item.getName()))).check(view, e);
             }
-          });
+        });
+    }
 
-  @Rule
-  public IntentsTestRule<SuperHeroesActivity> activityRule =
-          new IntentsTestRule<>(SuperHeroesActivity.class, true, false);
+    @Test
+    public void checkIfAvengersItemsHasBadges() {
+        givenThereIsMultipleSuperHeroes();
+        startActivity();
+        RecyclerViewInteraction.<SuperHero>onRecyclerView(withId(R.id.recycler_view)).withItems(repository.getAll()).check(new RecyclerViewInteraction.ItemViewAssertion<SuperHero>() {
+            @Override
+            public void check(SuperHero item, View view, NoMatchingViewException e) {
+                matches(hasDescendant(allOf(withId(R.id.iv_avengers_badge), withEffectiveVisibility((item.isAvenger() ? ViewMatchers.Visibility.VISIBLE : ViewMatchers.Visibility.GONE))))).check(view, e);
+            }
+        });
+    }
 
-  @Mock SuperHeroesRepository repository;
+    @Test
+    public void givenThereIsMultipleSuperHeroes() {
+        ArrayList<SuperHero> superHeros = new ArrayList<>();
+        superHeros.add(new SuperHero("Mostafa", null, false, "Has kick-ass coding skills"));
+        superHeros.add(new SuperHero("Coder 1", null, false, "Has kick-ass coding skills"));
+        superHeros.add(new SuperHero("Coder 2", null, true, "Has kick-ass coding skills"));
+        superHeros.add(new SuperHero("Coder 3", null, false, "Has kick-ass coding skills"));
+        when(repository.getAll()).thenReturn(superHeros);
+    }
 
-  @Test public void showsEmptyCaseIfThereAreNoSuperHeroes() {
-    givenThereAreNoSuperHeroes();
+    @Test
+    public void givenThereIsASuperHeroes() {
+        when(repository.getAll()).thenReturn(Arrays.asList(new SuperHero("Mostafa", null, false, "Has kick-ass coding " + "skills")));
+    }
 
-    startActivity();
+    private void givenThereAreNoSuperHeroes() {
+        when(repository.getAll()).thenReturn(Collections.<SuperHero>emptyList());
+    }
 
-    onView(withText("¯\\_(ツ)_/¯")).check(matches(isDisplayed()));
-  }
-
-  @Test public void showsTheToolbar(){
-    givenThereAreNoSuperHeroes();
-
-    startActivity();
-
-    //check if the toolbar is displayed and it has a childview with the mentioned text
-    onView(allOf(withId(R.id.toolbar), hasDescendant( withText("Kata Super Heroes")))).check(matches(isDisplayed()));
-  }
-
-  private void givenThereAreNoSuperHeroes() {
-    when(repository.getAll()).thenReturn(Collections.<SuperHero>emptyList());
-  }
-
-  private SuperHeroesActivity startActivity() {
-    return activityRule.launchActivity(null);
-  }
+    private SuperHeroesActivity startActivity() {
+        return activityRule.launchActivity(null);
+    }
 }
